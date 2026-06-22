278bc228b0413ebed57b403f2aab20cc83f6c329
mathewTheGreat <momanyimathew@gmail.com>
2026-06-22 19:16:33 +0300

    Fix preload ESM error: delete preload.ts, copy preload.js as CommonJS

--- electron/preload.ts
+++
@@ -1,7 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron'
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  getTheme: () => ipcRenderer.invoke('get-theme'),
-  setTheme: (theme: string) => ipcRenderer.invoke('set-theme', theme),
-  platform: process.platform,
-})
